6e0330c2b2051be5638542414688a9b76a3d0fcf
Roberto Verdini <Roberto Verdini>
2017-10-11 09:45:31 +0200

    Add project files.

---
+++ GtkTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using Gtk;
+
+namespace GtkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+
+            using (Window window = new Window("Gtk# Widget viewer"))
+            {
+                window.DeleteEvent += OnWindowDelete;
+                window.SetDefaultSize(800, 600);
+                window.ShowAll();
+
+                Application.Run();
+            }
+        }
+
+        static void OnWindowDelete(object o, DeleteEventArgs args)
+        {
+            Application.Quit();
+            args.RetVal = true;
+        }
+    }
+}
